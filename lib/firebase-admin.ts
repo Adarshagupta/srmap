ba221d85f7cc0adc4162e37da2fcad7d8289ebb3
Adarsh Gupta <blueadarsh1@gmail.com>
2024-12-25 11:09:56 +0530

    fix the error

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -1,14 +1,2 @@
-import * as admin from 'firebase-admin';
-
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-    }),
-  });
-}
-
-export const adminAuth = admin.auth();
-export const adminDb = admin.firestore(); 
+// This file is not needed for static exports as we're using client-side Firebase only
+// If you need server-side Firebase functionality in the future, you can re-enable this file 
